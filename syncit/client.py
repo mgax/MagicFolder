2101e54b7f4e429084eb17263a77adbeccfc96e5
Alex Morega <alex@grep.ro>
2010-06-21 00:37:48 +0300

    client-side logging

--- syncit/client.py
+++ syncit/client.py
@@ -1,9 +1,13 @@
 import os
 from os import path
 from subprocess import Popen, PIPE
+import logging
+from time import time
 
 import picklemsg
 from probity.walk import walk_path as probity_walk_path
+
+log = logging.getLogger('syncit.client')
 
 def client_sync(root_path, remote):
     private_path = path.join(root_path, '.syncit')
@@ -17,15 +21,26 @@ def client_sync(root_path, remote):
         last_version = 0
         os.makedirs(private_path)
 
+    log.debug("sync session, last_version=%d - listing local files",
+              last_version)
+
     remote.send('merge', last_version)
     msg, payload = remote.recv()
     assert msg == 'waiting_for_files'
+
+    bytes_count = 0
+    files_count = 0
+    time0 = time()
 
     root_name = path.basename(root_path)
     for event in probity_walk_path(root_path):
         file_path = event.path.split('/', 1)[1]
         if file_path.startswith('.syncit/'):
             continue
+
+        files_count += 1
+        if files_count % 100 == 0:
+            log.debug("still listing local files, %d so far", files_count)
 
         file_meta = {
             'path': file_path,
@@ -42,6 +57,11 @@ def client_sync(root_path, remote):
         #print 'sending data for %r' % file_path
         with open(event.fs_path, 'rb') as data_file:
             remote.send_file(data_file)
+            bytes_count += data_file.tell()
+
+    log.debug("finished sending files to server (%d bytes in %d seconds)",
+              bytes_count, int(time() - time0))
+    log.debug("we have %d files locally", files_count)
 
     remote.send('done')
 
@@ -51,6 +71,7 @@ def client_sync(root_path, remote):
             break
 
         elif msg == 'file_begin':
+            log.debug("new file %r", payload['path'])
             file_path = path.join(root_path, payload['path'])
             folder_path = path.dirname(file_path)
             if not path.isdir(folder_path):
@@ -60,15 +81,17 @@ def client_sync(root_path, remote):
                 remote.recv_file(local_file)
 
         elif msg == 'file_remove':
+            log.debug("removing file %r", payload)
             os.unlink(path.join(root_path, payload))
 
         else:
             assert False, 'unexpected message %r' % msg
 
     assert payload >= last_version
-    #print 'sync complete; now at version %d' % payload
     with open(last_sync_path, 'wb') as f:
         f.write("%d\n" % payload)
+
+    log.debug("sync complete, now at version %d", payload)
 
     remote.send('quit')
     assert remote.recv()[0] == 'bye'
@@ -83,6 +106,8 @@ def pipe_to_remote(remote_spec):
 def main():
     import sys
     assert len(sys.argv) == 3
+
+    logging.basicConfig(level=logging.DEBUG)
 
     root_path = sys.argv[1]
     remote = pipe_to_remote(sys.argv[2])
