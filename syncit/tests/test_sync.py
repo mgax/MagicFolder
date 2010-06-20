4512a684a0bafc4d6f1c604e9f7ead6b676345e7
Alex Morega <alex@grep.ro>
2010-06-20 21:39:41 +0300

    first test: initial sync with server

---
+++ syncit/tests/test_sync.py
@@ -0,0 +1,92 @@
+import unittest
+import tempfile
+import shutil
+import os
+from os import path
+import threading
+from Queue import Queue
+from hashlib import sha1
+
+from probity.backup import Backup
+
+from syncit.client import Client
+from syncit.server import Server
+from syncit.picklemsg import Remote
+
+def sha1hex(s):
+    return sha1(s).hexdigest()
+
+class TestRemote(Remote):
+    def __init__(self, in_queue, out_queue):
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+
+    def send(self, msg, payload=None):
+        self.out_queue.put( (msg, payload) )
+
+    def recv(self):
+        msg, payload = self.in_queue.get()
+        if msg == 'error':
+            print "error from remote endpoint\n%s" % payload
+        return msg, payload
+
+def do_server_loop(root_path, in_queue, out_queue):
+    Server(root_path, TestRemote(in_queue, out_queue)).loop()
+
+def do_client_sync(root_path, in_queue, out_queue):
+    Client(root_path, TestRemote(in_queue, out_queue)).sync()
+
+def do_client_server(client_root, server_root):
+    c2s = Queue()
+    s2c = Queue()
+    server_thread = threading.Thread(target=do_server_loop,
+                                     args=(server_root, c2s, s2c))
+    server_thread.start()
+    do_client_sync(client_root, s2c, c2s)
+    server_thread.join()
+
+class SyncTest(unittest.TestCase):
+    def setUp(self):
+        self.client_tmp_path = tempfile.mkdtemp()
+        self.client_root = path.join(self.client_tmp_path, 'repo')
+        self.server_root = tempfile.mkdtemp()
+        self.server_objects_path = path.join(self.server_root, 'objects')
+        self.server_versions_path = path.join(self.server_root, 'versions')
+        os.mkdir(self.server_objects_path)
+        os.mkdir(self.server_versions_path)
+
+    def run_loop(self):
+        do_client_server(self.client_root, self.server_root)
+
+    def tearDown(self):
+        shutil.rmtree(self.client_tmp_path)
+        shutil.rmtree(self.server_root)
+
+    def server_fixtures(self, files):
+        server_objs = Backup(self.server_objects_path)
+        with open(path.join(self.server_versions_path, '1'), 'wb') as index_f:
+            for file_path, file_data in files.iteritems():
+                index_f.write('%s: {sha1: %s, size: %d}\n' %
+                              (file_path, sha1hex(file_data), len(file_data)))
+                with server_objs.store_data(sha1hex(file_data)) as data_f:
+                    data_f.write(file_data)
+
+    def test_initial(self):
+        self.server_fixtures({
+            'path_one': "hello world",
+            'path_two': "hi there",
+        })
+        self.run_loop()
+
+        self.assertEqual(set(os.listdir(self.client_root)),
+                         set(['.syncit', 'path_one', 'path_two']))
+        with open(path.join(self.client_root, '.syncit/last_sync'), 'rb') as f:
+            self.assertEqual(f.read(), "1\n")
+        with open(path.join(self.client_root, 'path_one'), 'rb') as f:
+            self.assertEqual(f.read(), "hello world")
+        with open(path.join(self.client_root, 'path_two'), 'rb') as f:
+            self.assertEqual(f.read(), "hi there")
+
+
+if __name__ == '__main__':
+    unittest.main()
