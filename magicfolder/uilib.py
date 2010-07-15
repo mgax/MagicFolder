985513016e63d896923f1eb84a31f34a79dd0c51
Alex Morega <alex@grep.ro>
2010-07-15 09:57:51 +0300

    tiny library that prints fancy console text

---
+++ magicfolder/uilib.py
@@ -0,0 +1,63 @@
+import time
+import sys
+from contextlib import contextmanager
+
+codes = {
+    'red': '\033[91m',
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'default': '\033[0m',
+}
+
+class ColorfulUi(object):
+    def out(self, text):
+        sys.stdout.write(text)
+        sys.stdout.flush()
+
+    @contextmanager
+    def colored(self, color):
+        self.out(codes[color])
+        yield self.out
+        self.out(codes['default'])
+
+    @contextmanager
+    def status_line(self):
+        class state: count = 0
+        def clear():
+            if state.count:
+                self.out('\r' + ' ' * state.count + '\r')
+                state.count = 0
+
+        def line_printer(text):
+            clear()
+            assert len(text) < 80
+            state.count = len(text)
+            self.out(text)
+
+        yield line_printer
+        clear()
+
+class DummyUi(ColorfulUi):
+    def out(self, text):
+        pass
+
+
+def demo():
+    cui = ColorfulUi()
+
+    with cui.colored('red') as colored_text:
+        colored_text('hello ')
+    with cui.colored('green'):
+        colored_text('world')
+    cui.out('!\n')
+
+    with cui.status_line() as write_line:
+        write_line('asdf')
+        time.sleep(.2)
+        write_line('qwer')
+        time.sleep(.2)
+        write_line('lala')
+        time.sleep(.2)
+
+if __name__ == '__main__':
+    demo()
