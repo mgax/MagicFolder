5abc1af190ad44fde6a93125cbedd2ac6dac17bc
Alex Morega <alex@grep.ro>
2010-07-26 20:17:29 +0300

    long description in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
+try:
+    from os import path
+    with open(path.join(path.dirname(__file__), 'README.rst'), 'rb') as f:
+        long_description = f.read()
+except:
+    long_description = ""
+
 setup(
     name="MagicFolder",
     description="Synchronize local folder with a central repository",
+    long_description=long_description,
     version="0.2",
     license="MIT License",
     author="Alex Morega",
